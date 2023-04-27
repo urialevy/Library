28ae6a08bda353c3de178fd6bb2a3f68bda6f7e3
Uria <dorman010@gmail.com>
2023-04-27 13:56:36 +0100

    add html container, css grid, JS read library function

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read){
     myLibrary.push(new Book(title, author, pages, read))
 }
+addBookToLibrary("The Eye of the World", "Robert Jordan", 782,"read")
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 304,"read")
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald",208,"not read")
+
+console.log(myLibrary.forEach(readBook))
+function readBook(book) {
+    console.log(book)
+    return book
+}
