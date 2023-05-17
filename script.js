e7a4ceb3ad524a035552df851f234cef2a16ac2a
Uria <dorman010@gmail.com>
2023-05-17 23:34:37 +0100

    trying to get the script to read form elements as input, failing

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 // delcaring variables from the webpage
 
 window.onload = function() {
-const library = document.getElementById('libBody')
+library = document.getElementById('libBody')
 const newBookBtn = document.getElementById('newbook');
-let formArea = document.getElementById('newForm')
+formArea = document.getElementById('newForm')
 newBookBtn.addEventListener('click', () => addNewBook())
-for (let i = 0; i<myLibrary.length; i++) {
-    let row = library.insertRow(i+1)
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
-    let cell5 = row.insertCell(4);
-    let cell6 = row.insertCell(5);
-    let cell7 = row.insertCell(6);
-    cell1.innerHTML = myLibrary[i].title;
-    cell2.innerHTML = myLibrary[i].author;
-    cell3.innerHTML = myLibrary[i].pages;
-    if (myLibrary[i].read == true) {cell4.innerHTML = "Yes"} else {cell4.innerHTML = "No"}
-    cell5.innerHTML = `<td><button class="editbtn" id="edit_${i}">EDIT</button></td>`;
-    cell6.innerHTML = `<td><button class="deletebtn" id="delete_${i}">DELETE</button></td>`;
-}
-}
-
+    for (let i = 0; i<myLibrary.length; i++) {
+        let row = library.insertRow(i+1)
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+        let cell5 = row.insertCell(4);
+        let cell6 = row.insertCell(5);
+        let cell7 = row.insertCell(6);
+        cell1.innerHTML = myLibrary[i].title;
+        cell2.innerHTML = myLibrary[i].author;
+        cell3.innerHTML = myLibrary[i].pages;
+        if (myLibrary[i].read == true) {cell4.innerHTML = "Yes"} else {cell4.innerHTML = "No"}
+        cell5.innerHTML = `<td><button class="editbtn" id="edit_${i}">EDIT</button></td>`;
+        cell6.innerHTML = `<td><button class="deletebtn" id="delete_${i}">DELETE</button></td>`;
+    }}
 
 
 
@@ -37,8 +35,9 @@ function Book(title, author, pages, read) {
 
 }
 // add book to library function
-function addBookToLibrary(title, author, pages, read){
-    myLibrary.push(new Book(title, author, pages, read))
+function addBookToLibrary(title, author, pages, read){true
+    myLibrary.push(new Book(title, author, pages, read));
+    
 }
 
 // Adding some books 
@@ -55,12 +54,24 @@ addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald",208,false)
 function addNewBook() {
     newForm.innerHTML =`<form id="submitNew">
     <label for="Title">Title</label>
-    <input type="text" name="Title"> <br>
+    <input type="text" name="Title" id="title" placeholder="Title" required> <br>
     <label for="Author">Author</label>
-    <input type="text" name="Author"> <br>
+    <input type="text" name="Author" id="author" placeholder="Author" required> <br>
     <label for="Pages">Pages</label>
-    <input type="number" name="Pages"> <br>
-    <label for="Read">Read?</label>
-    <input type="checkbox"> <br>
+    <input type="number" name="Pages" id="pages" required> <br>
+    <label for="read">Read?</label>
+    <input type="checkbox" id="read"> <br>
     <input type="submit" id="submitBtn">
-</form>`}
+</form>`;
+submitBtn = document.getElementById('submitBtn')
+title = document.getElementById('title').value
+author = document.getElementById('author').value
+pages = document.getElementById('pages').value
+read = document.getElementById('read').checked
+form = document.getElementById('submitNew');
+form.addEventListener("submit", (e, i) => {e.preventDefault();
+addBookToLibrary(i)})
+// form.addEventListener('submit', (e, function)) => {e.preventDefault()}
+// submitBtn.addEventListener('submit',() => addBookToLibrary(title, author, pages, read))
+
+}
