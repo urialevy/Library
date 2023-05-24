835a795caf780d7c59606d1884613a8ab982cf7b
Uria <dorman010@gmail.com>
2023-05-24 22:28:25 +0100

    trying to figure out this JS bug, new rows keep getting created

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // delcaring variables from the webpage
-
 window.onload = function() {
 const library = document.getElementById('libBody')
-const newBookBtn = document.getElementById('newbook');
 const formArea = document.getElementById('newForm')
+const newBookBtn = document.getElementById('newbook');
 newBookBtn.addEventListener('click', () => addNewBook())
     for (let i = 0; i<myLibrary.length; i++) {
-        let row = library.insertRow(i+1)
+        let row = library.insertRow(-1)
         let cell1 = row.insertCell(0);
         let cell2 = row.insertCell(1);
         let cell3 = row.insertCell(2);
@@ -23,9 +22,8 @@ newBookBtn.addEventListener('click', () => addNewBook())
     }}
 
 
-
 // declaring library variable and the Book object
-let myLibrary = []
+const myLibrary = []
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -45,35 +43,32 @@ addBookToLibrary("The Eye of the World", "Robert Jordan", 782,true)
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 304, true)
 addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald",208,false)
 
-// create add new book form
+// create add new book form with its functionality
 function addNewBook() {
     document.getElementById('submitNew').style.visibility='visible'
-
 ;
 
 let submitBtn = document.getElementById('submitBtn')
 let form = document.getElementById('submitNew');
 form.addEventListener("submit", (e) => {e.preventDefault();
-let title = document.getElementById('title').value
-let author = document.getElementById('author').value
-let pages = Number(document.getElementById('pages').value)
-let read = document.getElementById('read').checked
-addBookToLibrary(title, author, pages, read)
-let row = library.insertRow(-1)
-let cell1 = row.insertCell(0);
-let cell2 = row.insertCell(1);
-let cell3 = row.insertCell(2);
-let cell4 = row.insertCell(3);
-let cell5 = row.insertCell(4);
-let cell6 = row.insertCell(5);
-let cell7 = row.insertCell(6);
-cell1.innerHTML = this.title.value;
-// cell2.innerHTML = myLibrary[-1].author;
-// cell3.innerHTML = myLibrary[-1].pages;
-// cell4.innerHTML = myLibrary[-1].read
-cell5.innerHTML = `<td><button class="editbtn" id="edit_${myLibrary.length}">EDIT</button></td>`;
-cell6.innerHTML = `<td><button class="deletebtn" id="delete_${myLibrary.length}">DELETE</button></td>`;
-
-
+    let title = document.getElementById('title').value
+    let author = document.getElementById('author').value
+    let pages = Number(document.getElementById('pages').value)
+    let read = document.getElementById('read').checked
+    addBookToLibrary(title, author, pages, read)
+        let row = library.insertRow(-1)
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+        let cell5 = row.insertCell(4);
+        let cell6 = row.insertCell(5);
+        let cell7 = row.insertCell(6);
+        cell1.innerHTML = this.title.value;
+        cell2.innerHTML = this.author.value;
+        cell3.innerHTML = this.pages.value;
+        cell4.innerHTML = this.read.checked;
+        cell5.innerHTML = `<td><button class="editbtn" id="edit_${myLibrary.length}">EDIT</button></td>`;
+        cell6.innerHTML = `<td><button class="deletebtn" id="delete_${myLibrary.length}">DELETE</button></td>`;
 submitNew.reset();
 document.getElementById('submitNew').style.visibility='hidden'})}
